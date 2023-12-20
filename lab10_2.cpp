3046246bfcb46799e1bc5f4a5a574afb58699f0f
Niphitpon Thosap <NiphitponT.63@cmu.ac.th>
2023-12-20 23:06:23 +0700

    ส่งงาน

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 
 int main (){
-    ifstream source;
-    ofstream dest;
-    source.open("cheerbook.txt");
-    dest.open("cheerbook_copy.txt");
-    
+	ifstream source;
+	ofstream dest;
+	source.open("cheerbook.txt");
+	dest.open("cheerbook_copy.txt");
+	
     string textline;
     dest << "-------------------- BOOM ---------------------\n";
     while(getline(source,textline)){
         dest << textline << "\n" ;
     }
     dest << "-------------------- HA!! ---------------------";
-
-
     source.close();
     dest.close();
-    return 0;
+	return 0;
 }
+
+    
